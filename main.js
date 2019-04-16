24c8de19eee73832c6285d19979693ee86ab958a
Marrero <0193605@students.dadeschools.net>
2019-04-16 14:08:11 -0400

    finshed html and started js

---
+++ main.js
@@ -0,0 +1,24 @@
+var panel = $("quiz-area");
+
+var countStartNumber = 30;
+
+// Question set
+var questions = [
+    
+    {
+        question: "What is the creator of smash bros race",
+        answers : ["Black", "white", "Japanese"],
+        correctAnswer : "Japanese",
+
+
+    }
+
+    {
+        question: "How many bosses are there in Breath of the wild",
+        answers : ["4", "5", "6"],
+        correctAnswer : "5",
+
+
+    }
+
+]
